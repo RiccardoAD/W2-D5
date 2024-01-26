93e74c792e734c17c395116b3c0733dd1b3c19b9
RiccardoAD <rick_95@libero.it>
2024-01-26 16:13:28 +0100

    second.commit

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -90,11 +90,28 @@ console.log(" Esecizio 6 ",cars);
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = []
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0]);
+  
+  
+}
 
+console.log("eserzio 7 ",justTrims);
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+ for (let i = 0; i < cars.length; i++) {
+   if( cars[i].color.startsWith("b")==true){
+     console.log("ES 7 frizz");
+
+
+   } else {
+    console.log("ES 7 Buzz");
+   }
+  
+ }
+  
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -102,7 +119,12 @@ const justTrims = []
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
-
+let i = 0;
+while (numericArray[i] !== 32 && i < numericArray.length) {
+    console.log(" Esercizio 9 ",numericArray[i]);
+    i++; 
+}
+ 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
